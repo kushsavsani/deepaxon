2521e9fb3fa05211951a951553c55585ca930d60
kushsavsani <kushs222@gmail.com>
2024-05-31 10:16:22 -0400

    On branch cmd_commands Your branch is up to date with 'deepaxon/cmd_commands'.
    Changes to be committed:
    	modified:   morphometrics/__main__.py

--- morphometrics/__main__.py
+++ morphometrics/__main__.py
@@ -1,8 +1,11 @@
+import os
 import morphometrics
 
-img_path = input("Input the path to the image: ")
+dir_path = input("Input the path to the folder of segmented images: ")
 output_dir = input("Input the path to the output folder: ")
-output_name = input("Input the name of the output file: ")
 
-morph_df = morphometrics.get_morphometrics(img_path)
-morphometrics.save_morphometrics(morph_df, output_dir, output_name)
+for img_name in os.listdir(dir_path):
+    img_path = os.path.join(dir_path, img_name)
+    output_name = img_name.split('.')[0]
+    morph_df = morphometrics.get_morphometrics(img_path)
+    morphometrics.save_morphometrics(morph_df, output_dir, output_name)
